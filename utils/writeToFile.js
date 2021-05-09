0b0aea8bfea7b7ed891e07695731b4d2aadd5291
Misha <73886737+misha244@users.noreply.github.com>
2021-05-09 12:27:41 +0100

    add callback and appendfile

--- utils/writeToFile.js
+++ utils/writeToFile.js
@@ -1,9 +1,16 @@
-constant fs = require("fs")
+const fs = require("fs");
+
 // TODO: Create a function to write README file
+const writeToFile = (fileName, generatedMarkdown) => {
+  const callback = (err) => {
+    if (err) {
+      console.log("An error occured!", err);
+    } else {
+      console.log("README successfully generated!");
+    }
+  };
 
-const writeToFile = (fileName, generateMarkdown) => {
-
-
+  fs.appendFile(`./${fileName}.md`, generatedMarkdown, callback);
 };
 
 module.exports = writeToFile;
